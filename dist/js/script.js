fc4db11524477915a75f71f44f12ec51e1f723a9
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-21 14:51:11 -0400

    21.10-dev-16

--- dist/js/script.js
+++ dist/js/script.js
@@ -27,9 +27,9 @@ API.Plugins.containers = {
 				},function(result) {
 					var dataset = JSON.parse(result);
 					if(dataset.success != undefined){
-						for(const [key, value] of Object.entries(dataset.output.results)){ API.Helper.set(API.Contents,['data','dom','containers',value.id],value); }
+						for(const [key, value] of Object.entries(dataset.output.dom)){ API.Helper.set(API.Contents,['data','dom','containers',value.id],value); }
 						for(const [key, value] of Object.entries(dataset.output.raw)){ API.Helper.set(API.Contents,['data','raw','containers',value.id],value); }
-						API.Builder.table(card.children('.card-body'), dataset.output.results, {
+						API.Builder.table(card.children('.card-body'), dataset.output.dom, {
 							headers:dataset.output.headers,
 							id:'ContainersIndex',
 							modal:true,
@@ -165,7 +165,7 @@ API.Plugins.containers = {
 														if(result.charAt(0) == '{'){
 															var newdataset = JSON.parse(result);
 															if(typeof dataset.success !== 'undefined'){
-																newValue[key] = newdataset.output.results[key];
+																newValue[key] = newdataset.output.dom[key];
 																API.GUI.insert(newValue);
 															} else {
 																newValue[key] = dataset.output.container.dom[key];
